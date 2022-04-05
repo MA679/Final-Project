93fb94be7cdc55e20137eb543c441790a1a3d495
James <yfhe@bu.edu>
2022-04-05 13:38:51 -0400

    all mice uploaded

--- James_Try.R
+++ James_Try.R
@@ -7,9 +7,9 @@ library(ggplot2)
 Mouse_412_behavior=readMat("Data/Opp_Sex/608102_412/Day_1/Trial_002_0/binned_behavior.mat")
 M412_b <- as.data.frame(t(Mouse_412_behavior$binned.behavior))
 M412_b <- M412_b %>% mutate(Act =
-                     case_when(V1 == 1 ~ "A", 
-                               V2 == 1 ~ "B",
-                               V1 == V2 ~ "C")
+                     case_when(V1 == 1 ~ "male", 
+                               V2 == 1 ~ "female",
+                               V1 == V2 ~ "no touch")
 )
 #View(M412_b)
 table(M412_b$Act)
@@ -23,16 +23,26 @@ M412_z$sum <- rowSums(M412_z, na.rm = TRUE)
 
 
 total412 <- cbind(M412_b, M412_z)
-value_412 <- total412[,c(1,2,50,51)]
+value_412 <- total412[,c(3,52,53)]
 value_412$time <- c(1:6300)
 #View(value_412)
 
 #Time Series EDA for 412
 ggplot(data = value_412)+
   geom_line(mapping = aes(x = time, y = avg))+
-  geom_point(mapping = aes(x = time, y = V1), col = 'Red', size = 0.01)+
+  geom_point(mapping = aes(x = time, y = V1), size = 0.01)+
   geom_point(mapping = aes(x = time, y = V2), col = 'Blue', size = 0.01)
 
+ggplot(data = value_412)+
+  geom_line(mapping = aes(x = time, y = sum))+
+  geom_point(mapping = aes(x = time, y = sum, col = Act), size = 0.01)
+
+ggplot(data = value_412)+
+  geom_line(mapping = aes(x = time, y = avg), col = 'Grey')+
+  geom_point(mapping = aes(x = time, y = avg, col = Act, shape = Act),size = 0.5)
+
+
+#---------------------------------------------------------------------------
 ##Mouse 417
 Mouse_417_behavior=readMat("Data/Opp_Sex/608103_417/Day_2/Trial_002_0/binned_behavior.mat")
 M417_b <- as.data.frame(t(Mouse_417_behavior$binned.behavior))
@@ -55,6 +65,8 @@ ggplot(data = value_417)+
   geom_point(mapping = aes(x = time, y = V1), col = 'Red', size = 0.01)+
   geom_point(mapping = aes(x = time, y = V2), col = 'Blue', size = 0.01)
 
+#---------------------------------------------------------------------------------------
+
 ##mouse 416
 Mouse_416_behavior=readMat("Data/Opp_Sex/608103_416/Day_2/Trial_002_0/binned_behavior.mat")
 M416_b <- as.data.frame(t(Mouse_416_behavior$binned.behavior))
@@ -70,6 +82,8 @@ M416_z <- as.data.frame(Mouse_416_zscore$binned.zscore)
 M416_z$avg <- rowMeans(M416_z, na.rm=TRUE)
 M416_z$sum <- rowSums(M416_z, na.rm = TRUE)
 #View(M416_z)
+
+#-----------------------------------------------------------------------------------------
 
 ##Mouse 409
 Mouse_409_behavior=readMat("Data/Opp_Sex/608034_409/Day_1/Trial_002_0/binned_behavior.mat")
@@ -87,5 +101,39 @@ M409_z$avg <- rowMeans(M409_z, na.rm=TRUE)
 M409_z$sum <- rowSums(M409_z, na.rm = TRUE)
 #View(M409_z)
 
-#????
+#------------------------------------------------------------------------------------------------
+##Mouse 414
+Mouse_414_behavior=readMat("Data/Opp_Sex/608102_414/Day_2/Trial_002_0/binned_behavior.mat")
+M414_b <- as.data.frame(t(Mouse_414_behavior$binned.behavior))
+M414_b <- M414_b %>% mutate(Act =
+                              case_when(V1 == 1 ~ "male", 
+                                        V2 == 1 ~ "female",
+                                        V1 == V2 ~ "no touch")
+)
+#View(M414_b)
 
+Mouse_414_zscore=readMat("Data/Opp_Sex/608102_414/Day_2/Trial_002_0/binned_zscore.mat")
+M414_z <- as.data.frame(Mouse_414_zscore$binned.zscore)
+M414_z$avg <- rowMeans(M414_z, na.rm=TRUE)
+M414_z$sum <- rowSums(M414_z, na.rm = TRUE)
+#View(M414_z)
+
+#---------------------------------------------------------------------------------------
+
+##Mouse 418
+Mouse_418_behavior=readMat("Data/Opp_Sex/608103_418/Day_2/Trial_002_0/binned_behavior.mat")
+M418_b <- as.data.frame(t(Mouse_418_behavior$binned.behavior))
+M418_b <- M418_b %>% mutate(Act =
+                              case_when(V1 == 1 ~ "male", 
+                                        V2 == 1 ~ "female",
+                                        V1 == V2 ~ "no touch")
+)
+#View(M418_b)
+
+Mouse_418_zscore=readMat("Data/Opp_Sex/608103_418/Day_2/Trial_002_0/binned_zscore.mat")
+M418_z <- as.data.frame(Mouse_418_zscore$binned.zscore)
+M418_z$avg <- rowMeans(M418_z, na.rm=TRUE)
+M418_z$sum <- rowSums(M418_z, na.rm = TRUE)
+#View(M418_z)
+
+#-----------------------------------------------------------------------------------------
